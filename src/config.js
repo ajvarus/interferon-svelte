2fe1428f7a800da785393cd7d214a75d5e5edd0a
Arjun Sen <arjunsen1997@gmail.com>
2024-06-07 19:37:27 +0100

    Updated config file

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 // src/config.js
 
 export const INTF_BASE_URL = "https://interferon-fastapi-dev.onrender.com";
+//export const INTF_BASE_URL = "http://localhost:8000";
 export const INTF_AUTH_ENDPOINT = `${INTF_BASE_URL}/auth`;
+export const INTF_GRAPHQL_ENDPOINT = `${INTF_BASE_URL}/graphql`;
